2e53c06e699f5d27190c497322ebf3f11c551066
Pjvl99 <61527863+Pjvl99@users.noreply.github.com>
2021-02-14 17:39:38 -0600

    Delete Segundaentrega.py

--- Segundaentrega.py
+++
@@ -1,9 +0,0 @@
-import time
-numero = int(input("Bienvenido introduzca la cantidad de numeros naturales a sumarse: "))
-def sumador(num):
-    suma = 0
-    for x in range(num+1):
-        suma = suma + x
-    print("El resultado es:", suma)
-    return suma
-sumador(numero)
